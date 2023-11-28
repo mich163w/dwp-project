bddc22d65d84ea45a57940dcf744e44e73f9e174
mich163w <71692337+mich163w@users.noreply.github.com>
2023-11-28 08:54:34 +0100

    Unblock
    Unblock og block af user

---
+++ logic/block.php
@@ -0,0 +1,53 @@
+<?php
+require("../DB/connection.php");
+
+if (isset($_GET['Username'])) {
+    $Username = $_GET['Username'];
+
+    // Hent brugerens blokeringsstatus
+    $getProfileQuery = "SELECT isBlocked FROM `Profile` WHERE Username = ?";
+    $stmtGetProfile = mysqli_prepare($conn, $getProfileQuery);
+    mysqli_stmt_bind_param($stmtGetProfile, "s", $Username);
+
+    if (!mysqli_stmt_execute($stmtGetProfile)) {
+        die("Error fetching user profile: " . mysqli_stmt_error($stmtGetProfile));
+    }
+
+    mysqli_stmt_bind_result($stmtGetProfile, $isBlocked);
+    mysqli_stmt_fetch($stmtGetProfile);
+    mysqli_stmt_close($stmtGetProfile);
+
+    // Opdater brugerens blokeringsstatus
+    if ($isBlocked == 1) {
+        // Brugeren er blokeret, så unblock den
+        $updateProfileQuery = "UPDATE `Profile` SET isBlocked = 0 WHERE Username = ?";
+        $stmtProfile = mysqli_prepare($conn, $updateProfileQuery);
+        mysqli_stmt_bind_param($stmtProfile, "s", $Username);
+
+        if (!mysqli_stmt_execute($stmtProfile)) {
+            die("Error unblocking user: " . mysqli_stmt_error($stmtProfile));
+        } else {
+            echo "User unblocked successfully";
+        } 
+
+        mysqli_stmt_close($stmtProfile);
+    } else {
+        // Brugeren er ikke blokeret, så bloker den
+        $updateProfileQuery = "UPDATE `Profile` SET isBlocked = 1 WHERE Username = ?";
+        $stmtProfile = mysqli_prepare($conn, $updateProfileQuery);
+        mysqli_stmt_bind_param($stmtProfile, "s", $Username);
+
+        if (!mysqli_stmt_execute($stmtProfile)) {
+            die("Error blocking user: " . mysqli_stmt_error($stmtProfile));
+        } else {
+            echo "User blocked successfully";
+        } 
+
+        mysqli_stmt_close($stmtProfile);
+    }
+} else {
+    header("Location: index.php");
+}
+
+mysqli_close($conn);
+?>
--- views/edit.php
+++ views/edit.php
@@ -19,16 +19,18 @@ if (!isset($_SESSION['admin']) || $_SESSION['admin'] !== 'admin') {
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Edit Page</title>
 </head>
+
 <body>
-<div class="logout">
+    <div class="logout">
         <a href="logout.php"> <button class="logout-btn"> Log ud</button></a>
     </div>
     <h2>Hello Admin</h2>
     <p>You can edit stuff here</p>
-    
+
     <!-- Redigeringsknappen vises kun for administratorer -->
     <?php if (isset($_SESSION['admin']) && $_SESSION['admin'] === 'admin') { ?>
         <div class="edit-container">
@@ -40,90 +42,97 @@ if (!isset($_SESSION['admin']) || $_SESSION['admin'] !== 'admin') {
                         <span onclick="document.getElementById('id01').style.display='none'" class="w-button w-display-topright">&times;</span>
 
                         <?php
-                        $query = "SELECT *, isBlocked FROM `Profile`";
-                        $result = mysqli_query($conn, $query);
+$query = "SELECT *, isBlocked FROM `Profile`";
+$result = mysqli_query($conn, $query);
 
-                        while ($row = mysqli_fetch_array($result)) {
-                        ?>
-                            <div class="user-info">
-                                <span>
-                                    User: <?php echo $row['Username']; ?>
-                                    <?php if ($row['isBlocked'] == 1) {
-                                        echo "<span class='blocked'>(Blocked)</span>";
-                                    } else {
-                                        echo "(Not Blocked)";
-                                    } ?>
-                                </span>
-                                <a href='block.php?Username=<?php echo $row['Username']; ?>'>Block</a>
-                            </div>
-                        <?php
-                        }
-                        mysqli_close($conn);
-                        ?>
+while ($row = mysqli_fetch_array($result)) {
+?>
+    <div class="user-info">
+        <span>
+            User: <?php echo $row['Username']; ?>
+            <?php if ($row['isBlocked'] == 1) {
+                echo "<span class='blocked'>(Blocked)</span>";
+            } else {
+                echo "(Not Blocked)";
+            } ?>
+        </span>
+        <?php if ($row['isBlocked'] == 1) { ?>
+            <a href='../logic/block.php?Username=<?php echo $row['Username']; ?>&action=unblock'>Unblock</a>
+        <?php } else { ?>
+            <a href='../logic/block.php?Username=<?php echo $row['Username']; ?>&action=block'>Block</a>
+        <?php } ?>
+    </div>
+<?php
+}
+mysqli_close($conn);
+?>
+
                     </div>
                 </div>
             </div>
         </div>
     <?php } ?>
 </body>
-</html>    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet">
+
+</html>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet">
 
 
 <style>
-     body {
-            font-family: 'Montserrat', sans-serif;
-            padding: 0;
-            background-image: url(./img/DWPBaggrund.jpg);
-            background-size: cover;
-        }
-        .logout {
-            display: flex;
-            justify-content: flex-end;
-            padding: 10px;
-        }
+    body {
+        font-family: 'Montserrat', sans-serif;
+        padding: 0;
+        background-image: url(../BGimg/DWPBaggrund.jpg);
+        background-size: cover;
+    }
 
-        .logout-btn {
-            background-color: #ddb3b3;
-            color: #fff;
-            padding: 10px;
-            border: none;
-            border-radius: 10px;
-            cursor: pointer;
-        }
+    .logout {
+        display: flex;
+        justify-content: flex-end;
+        padding: 10px;
+    }
 
-        .edit-container {
-            display: flex;
-            flex-direction: column;
-           
-        }
+    .logout-btn {
+        background-color: #ddb3b3;
+        color: #fff;
+        padding: 10px;
+        border: none;
+        border-radius: 10px;
+        cursor: pointer;
+    }
 
-        .edit-btn {
-            width: 20%;
-            background-color: #ddb3b3;
-            color: #fff;
-            padding: 15px;
-            border: none;
-            border-radius: 10px;
-            cursor: pointer;
-            margin-top: 20px;
-        }
+    .edit-container {
+        display: flex;
+        flex-direction: column;
 
-        .user-info {
-            margin-top: 20px;
-            background-color: #f0f0f0;
-            padding: 15px;
-            border-radius: 10px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            width: 30%;
-        }
+    }
 
-        .blocked {
-            color: red;
-            font-weight: bold;
-        }    
+    .edit-btn {
+        width: 20%;
+        background-color: #ddb3b3;
+        color: #fff;
+        padding: 15px;
+        border: none;
+        border-radius: 10px;
+        cursor: pointer;
+        margin-top: 20px;
+    }
 
-        </style>
+    .user-info {
+        margin-top: 20px;
+        background-color: #f0f0f0;
+        padding: 15px;
+        border-radius: 10px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 30%;
+    }
+
+    .blocked {
+        color: red;
+        font-weight: bold;
+    }
+</style>
