6ac38326b2c289ba54ec74c19360b5f972eca5d0
mich163w <71692337+mich163w@users.noreply.github.com>
2023-12-04 10:41:13 +0100

    reChaptcha

--- views/login.php
+++ views/login.php
@@ -40,6 +40,13 @@ if (isset($_POST['submit'])) {
 
 <head>
     <meta http-equiv="Content-Type" content="text/html" />
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet">
+
+    <script src="https://www.google.com/recaptcha/api.js"></script>
 </head>
 
 <body>
@@ -51,13 +58,17 @@ if (isset($_POST['submit'])) {
     ?>
 
     <form action="" method="post">
+     
         <h2>Login</h2>
-        <h4>Username:</h4>
-        <input type="text" name="Username" maxlength="30" />
-        <h4>Password:</h4>
-        <input type="password" name="Pass" maxlength="30" />
-        <a href="./newuser.php"><p>Or sign up</p></a>
-        <input type="submit" name="submit" value="Login" />
+    <h4>Username:</h4>
+    <input type="text" name="Username" maxlength="30" />
+    <h4>Password:</h4>
+    <input type="password" name="Pass" maxlength="30" />
+    <a href="./newuser.php"><p>Or sign up</p></a>
+    <!-- Add reCAPTCHA widget to the login submit button -->
+    <div class="g-recaptcha" data-sitekey="6LfySSUpAAAAANqdMOMSVGqB4AsRkYpEG13pCqxM" data-action="LOGIN"></div>
+    <br/>
+    <input type="submit" name="submit" value="Login" />
     </form>
 </body>
 
@@ -65,12 +76,16 @@ if (isset($_POST['submit'])) {
 
 
 
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet">
+
+
+   <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+    async defer>
+
+function onSubmit(token) {
+      // Handle the token, for example, submit the form
+      document.getElementsByTagName("form")[0].submit();
+    }
+ </script>
     <style>
         body {
             font-family: 'Montserrat', sans-serif;
