43051a9265d90b99c497afa5902e40532e842866
mich163w <71692337+mich163w@users.noreply.github.com>
2023-12-08 11:11:33 +0100

    last login

--- views/login.php
+++ views/login.php
@@ -4,6 +4,7 @@ require("../DB/DBcon.php");
 spl_autoload_register(function ($class) {
     include "../classes/" . $class . ".php";
 });
+
 $session = new SessionHandle();
 
 // Check if the user is already logged in
@@ -20,14 +21,17 @@ if (isset($_POST['submit'])) {
     $Username = $_POST['Username'];
     $Pass = $_POST['Pass'];
     if ($Username === 'admin' && $Pass === 'adminpass') {
-        // Opdater last login i databasen
+        // Hent profilens ID baseret på brugernavnet
         $db = new DbCon();
-        $profileId = $db->getProfileIdByUsername($Username); // Gem ID'et i variabel
+        $profileId = $db->getProfileIdByUsername($Username);
+
+        // Opdater sidste login i databasen
         $db->updateLastLogin($profileId);
 
         $session->logged_in($Username); // Log ind som administrator
         $_SESSION['admin'] = $Username; // Gem admin-session
         $redirect = new Redirector("edit.php"); // Omdirigér til admin-siden
+
         // Check if the login attempt was successful
         if ($session->logged_in()) {
             $redirect = new Redirector("index.php");
@@ -37,9 +41,10 @@ if (isset($_POST['submit'])) {
         }
     }
 }
-
-
 ?>
+
+
+
 
 
 
