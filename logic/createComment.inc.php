d8ef733e62a4bb20b4fb00919749c61c10ca3b16
mich163w <71692337+mich163w@users.noreply.github.com>
2023-12-11 09:14:16 +0100

    comment

---
+++ logic/createComment.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+require("../DB/DBCon.php");
+
+    if (isset($_POST['commentSubmit'])) {
+        $CommentText = htmlspecialchars(trim($_POST['CommentText']));
+        $dato = htmlspecialchars(trim($_POST['dato']));
+        $MediaCommentFK = $_POST['MediaID'];
+        $ProfileFK = 1; // Dette er til test, det skal ændres til brugeren som er logget ind
+
+        if($conn){
+
+            $stmt = $conn->prepare("INSERT INTO Comment (CommentText, dato, MediaCommentFK, ProfileFK) VALUES (?, ?, ?, ?)");
+            $stmt->bind_param("ssii", $CommentText, $dato, $MediaCommentFK, $ProfileFK);
+            
+            $stmt->execute();
+            $stmt->close();
+            $conn->close();
+            
+            //header( "Location: ./index.php" );
+
+        } else {
+            die("error");
+        }
+    } 
--- logic/getComment.inc.php
+++ logic/getComment.inc.php
@@ -1,34 +1,44 @@
 <?php
-require("connection.php");
+
 // Antag, at $mediaCommentFK er din MediaID-værdi (Kun brugt af Ajax)
 if (isset($_GET["MediaID"])) {
+
     $mediaCommentFK = $_GET["MediaID"];
+
     // Inkluder databaseforbindelse
-    require_once("connection.php");
+    require_once("../DB/DBCon.php");
+    
     // Forbered og udfør det forberedte udsagn
-    $sql = "SELECT Profile.Username, Profile.Avatar, Comment.CommentText, Comment.dato
+    $sql = "SELECT Profile.Username, Profile.Avatar, Comment.CommentText, Comment.dato 
             FROM Media
             JOIN Comment ON Comment.MediaCommentFK = Media.MediaID
             JOIN Profile ON Profile.ProfileID = Comment.ProfileFK
             WHERE Comment.MediaCommentFK = ?";
+    
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $mediaCommentFK); // "i" angiver, at det er en integer-værdi
     $stmt->execute();
     $result = $stmt->get_result();
+    
     // Håndter resultatet (du kan tilpasse dette baseret på dine behov)
     while ($row = $result->fetch_assoc()) {
         echo "<div class='commment-box'>";
         echo "<div class='userBox'";
         echo "<img id='commentAvatar' src='" . $row['Avatar'] . "' alt='Avatar'><br>";
-        echo "<span class='userNameComment'>" . $row['Username'] . "</span><br>";
+        echo "<span class='userNameComment'>" . $row['Username'] . "</span><br>";    
         echo "</div>";
-        echo "<span class='datoComment'>" . $row['dato'] . "</span><br>";
-        echo "<span class='msgComment'>" . nl2br($row['CommentText']); "</span>";
+        echo "<span class='datoComment'>" . $row['dato'] . "</span><br>";    
+        echo "<span class='msgComment'>" . nl2br($row['CommentText']); "</span>";    
         echo "</div>";
     }
+    
+
 }
+
 ?>
+
 <style>
+
     .commment-box{
         border: 1px solid gray;
         width: 95%;
@@ -37,28 +47,34 @@ if (isset($_GET["MediaID"])) {
         margin-bottom: 1%;
         border-radius: 20px;
     }
+​
     .userBox {
         display: flex;
         align-items: center;
         gap: 2%;
     }
+
     #commentAvatar {
         height: 40px;
         width: 40px;
         background-color: red;
         border-radius: 980px;
     }
+
     .userNameComment {
         color:grey;
         opacity: 0.9;
         margin-left: 5%;
     }
+
     .datoComment {
         color:grey;
         opacity: 0.9;
         font-size: 12px;
     }
+
     .msgComment {
         color:black;
     }
+
 </style>
